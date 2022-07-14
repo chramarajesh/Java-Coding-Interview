cbf8b34d5bd4c94152edc31e3befbd9d8a32871f
Rama Rajesh Chittavarjula <Rajesh.Chittavarjula@ATMECS.CORP>
2022-07-14 17:03:14 +0530

    Committed the String Reverse Program using java

---
+++ src/main/java/com/code/practice/StringReverse.java
@@ -0,0 +1,14 @@
+package com.code.practice;
+public class StringReverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String reverse = "";
+		String readyToReverse = "Hello How Are you";
+		for (int i = readyToReverse.length() - 1; i >= 0; i--) {
+			reverse = reverse + readyToReverse.charAt(i);
+		}
+		System.out.println(reverse);
+	}
+
+}
